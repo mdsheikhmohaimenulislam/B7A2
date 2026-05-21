aaaf3bb621d7853d032d7956c7f4d420585af620
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-05-21 12:44:15 +0600

    Add User interface and createUserRequest type definition done

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,7 +1,23 @@
-// export const role = {
-//   maintainer: "maintainer",
-//   contributor: "contributor",
-// } as const;
+export const role = {
+  maintainer: "maintainer",
+  contributor: "contributor",
+} as const;
 
-// export type Role = (typeof role)[keyof typeof role];
+export type Role = (typeof role)[keyof typeof role];
 
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface CreateUserRequest {
+  name: string;
+  email: string;
+  password: string;
+  role?: Role;
+}
